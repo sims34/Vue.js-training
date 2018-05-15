d21985dadbe6ce2776f977cca14a48c44b2c4431
sims34 <maxfoerster86@gmail.com>
2018-05-15 18:32:13 +0200

    add @ notation + function, change name img (lesson 5)

--- main.js
+++ main.js
@@ -2,8 +2,30 @@ var app = new Vue({
     el:'#app',
     data:{
         product:'Socks',
-       image: './assets/vmSocks-green-onWhite.jpg',
-       link: 'https://blackstreet.com'
-
+       image: './assets/vmSocks-green.jpg',
+       inStock: true,
+       details :[ "80 % cotton","20 % polyester","Gender-Neutral"],
+       variants:[
+           {
+             varianteId : 2013,
+             varianteColor : 'Green',
+             varianteImage : './assets/vmSocks-green.jpg'
+           },
+           {
+             varianteId : 2154,
+             varianteColor : 'Blue',
+             varianteImage :'./assets/vmSocks-blue.jpg' 
+           }
+       ],
+       cart : 0
+    },
+    methods :{
+        addToCart : function(){ 
+            this.cart +=1
+        },
+        updateImage(varianteImage) {  // ES6 notation
+            this.image = varianteImage
+        }
     }
+   
 })
