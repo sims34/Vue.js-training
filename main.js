0608c5c0106226ece4950732bf7bd04aa3eddb48
simonbensoussan <simonbensoussan.sb@gmail.com>
2018-05-22 23:32:59 +0200

    add image() && inStock()

--- main.js
+++ main.js
@@ -4,19 +4,22 @@ var app = new Vue({
     data:{
        product:'Socks',
        brand : 'Vue Mastery',
-       image: './assets/vmSocks-green.jpg',
-       inStock: true,
+      // image: './assets/vmSocks-green.jpg',
+    //    inStock: true,
+       selectVariant: 0,
        details :[ "80 % cotton","20 % polyester","Gender-Neutral"],
        variants:[
            {
              varianteId : 2013,
              varianteColor : 'Green',
-             varianteImage : './assets/vmSocks-green.jpg'
+             varianteImage : './assets/vmSocks-green.jpg',
+             varianteQuantity: 255
            },
            {
              varianteId : 2154,
              varianteColor : 'Blue',
-             varianteImage :'./assets/vmSocks-blue.jpg' 
+             varianteImage :'./assets/vmSocks-blue.jpg',
+             varianteQuantity:0
            }
        ],
        cart : 0
@@ -25,13 +28,23 @@ var app = new Vue({
         addToCart : function(){ 
             this.cart +=1
         },
-        updateImage(varianteImage) {  // ES6 notation
-            this.image = varianteImage
+        updateImage(index) {  // ES6 notation
+            this.selectVariant = index
+            
         }
     },
     computed :{
         title(){
             return this.brand + ' -- '+ this.product
+        },
+        image(){
+            return this.variants[this.selectVariant].varianteImage
+            // ==> this.variants == the array
+            //==> this.variants[this.selectVariant] == array[0], array[1]
+        },
+        //boolean property if inStock == true
+        inStock(){ 
+            return this.variants[this.selectVariant].varianteQuantity
         }
     }
    
