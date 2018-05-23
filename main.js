74b117a391f388801f5f76e09e93498b4d8c93c5
simonbensoussan <simonbensoussan.sb@gmail.com>
2018-05-23 15:53:21 +0200

    lesson 8 component

--- main.js
+++ main.js
@@ -1,42 +1,70 @@
 //creation component
 Vue.component('product',{
+    props :{
+// props allow to make a connection between the parent (root) and the child (component)              
+// can be write ['message'] => in the template : <span>{{ message}}</span> => html : <product message ="Hello !"></product>
+// object syntaxe :
+            prenium:{
+                type : Boolean,
+                required: true
+            }
+    },
     // 7 ways to write template : https://medium.com/js-dojo/7-ways-to-define-a-component-template-in-vuejs-c04e0c72900d
-    template: ' <div></div> ', 
+    // back ticks => alt gr + 7
+    template: ` 
+            <div class="product">
+                
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+        
+            <div class="product-info">
+                    <h1>{{ title }}</h1>
+                    <p v-if= "inStock">In Stock</p>
+                    <p v-else>Out Stock</p>
+                    <p> Shipping price : {{ shipping }}</p>
+                    <ul>
+                        <li v-for="detail in details">{{ detail }}</li>
+                    </ul>
+
+                <div class="color-box"
+                    v-for=" v in variants" 
+                    :key=" v.varianteId"
+                    :style="{backgroundColor : v.varianteColor}"
+                    @mouseover="updateImage(v.varianteImage)">
+                </div>
+                <button v-on:click="addToCart"
+                        :disabled="! inStock"
+                        :class="{disabledButton : ! inStock}">Add to cart</button>
+                <div class="cart">
+                    <p>Cart ({{cart}})</p>
+                </div>
+            </div>
+        </div>
+    `,  
     
     data(){         //object
         return{
-            // write your stuff ...
+            product:'Socks',
+            brand : 'Vue Mastery',
+            image: './assets/vmSocks-green.jpg',
+            inStock: true,
+            details :[ "80 % cotton","20 % polyester","Gender-Neutral"],
+            variants:[
+                {
+                  varianteId : 2013,
+                  varianteColor : 'Green',
+                  varianteImage : './assets/vmSocks-green.jpg'
+                },
+                {
+                  varianteId : 2154,
+                  varianteColor : 'Blue',
+                  varianteImage :'./assets/vmSocks-blue.jpg' 
+                }
+            ],
+            cart : 0
         } 
         
-    },
-    methods :{
-        // write your stuff ...
-    },
-    computed :{
-        // write your stuff ...
-    },
-})
-var app = new Vue({
-    el:'#app',
-    data:{
-       product:'Socks',
-       brand : 'Vue Mastery',
-       image: './assets/vmSocks-green.jpg',
-       inStock: true,
-       details :[ "80 % cotton","20 % polyester","Gender-Neutral"],
-       variants:[
-           {
-             varianteId : 2013,
-             varianteColor : 'Green',
-             varianteImage : './assets/vmSocks-green.jpg'
-           },
-           {
-             varianteId : 2154,
-             varianteColor : 'Blue',
-             varianteImage :'./assets/vmSocks-blue.jpg' 
-           }
-       ],
-       cart : 0
     },
     methods :{
         addToCart : function(){ 
@@ -49,7 +77,24 @@ var app = new Vue({
     computed :{
         title(){
             return this.brand + ' -- '+ this.product
+        },
+        shipping(){
+            if(this.prenium){
+                return "Free"
+            }
+            return 2.99
+           
         }
+    },
+})
+
+//root element of Vuejs it become the 'Parent' of the application
+// the component it's the 'child'
+var app = new Vue({
+    el:'#app',
+    data :{
+        prenium : true
     }
+    
    
 })
